7e7cf8c050e774f186c47c77ce14338b0cb0f550
Shubham Mali <shubhamvmali09@gmail.com>
2025-08-03 19:05:36 +0530

    fix: Correct CORS origin URL format

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       'http://localhost:5173',
-      'https://live-polling-system-shubh.netlify.app/',
+      'https://live-polling-system-shubh.netlify.app',
     ],
     methods: ['GET', 'POST'],
   },
