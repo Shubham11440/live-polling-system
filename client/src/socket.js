5b18f24c2c493ed1dfc08903396519983283d100
Shubham Mali <shubhamvmali09@gmail.com>
2025-08-03 19:14:17 +0530

    debug: Log socket connection URL

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,5 @@
 import { io } from 'socket.io-client'
 
 const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:4000'
+console.log('Attempting to connect to Socket.io server at:', URL)
 export const socket = io(URL)
